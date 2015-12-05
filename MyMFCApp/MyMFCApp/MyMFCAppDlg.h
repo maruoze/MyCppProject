7320ee124313ae2e76cc054b5eec33b32362dd6c
MaRuoze <51899608@qq.com>
2015-12-05 19:03:41 +0800

    修改忽略 提交MFC

---
+++ MyMFCApp/MyMFCApp/MyMFCAppDlg.h
@@ -0,0 +1,34 @@
+
+// MyMFCAppDlg.h : 头文件
+//
+
+#pragma once
+
+
+// CMyMFCAppDlg 对话框
+class CMyMFCAppDlg : public CDialogEx
+{
+// 构造
+public:
+	CMyMFCAppDlg(CWnd* pParent = NULL);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MYMFCAPP_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
